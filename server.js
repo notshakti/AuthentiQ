f047ed821e63b1b5c25e14d6abc2b0bb1cb93bd2
Sam-Andrew-1227 <samandrewbritto@gmail.com>
2025-12-21 16:21:50 +0530

    c

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/api/certificates/:hash', async (req, res) => {
 });
 
 // Catch-all route for SPA
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
